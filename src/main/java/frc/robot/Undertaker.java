434afba363991b34e7b8c09744239f74ee03d4fc
Black Hole <ups1675@gmail.com>
2024-01-27 10:59:26 -0600

    updated code review stuff.

--- src/main/java/frc/robot/Undertaker.java
+++ src/main/java/frc/robot/Undertaker.java
@@ -9,35 +9,12 @@ public class Undertaker extends SubsystemBase{
     
     private CANSparkMax intakeMotorOne = new CANSparkMax( Constants.Undertaker.INTAKE_MOTOR_ONE, MotorType.kBrushless);
     private CANSparkMax intakeMotorTwo = new CANSparkMax( Constants.Undertaker.INTAKE_MOTOR_TWO, MotorType.kBrushless);
-    
-    public class Undertakers {
-      public static final int INTAKE_MOTOR_ONE = 9;
-      public static final int INTAKE_MOTOR_TWO = 10;
-    }
 
-    public void undertakerControls(double speed){
-        //stop
-        intakeMotorOne.set(0);
-        intakeMotorTwo.set(0);
+    public class Undertakers {}
 
-        //forwards
-        intakeMotorOne.set(speed);
-        intakeMotorTwo.set(speed);
-
-        //backwards
-        intakeMotorOne.set(speed * -1);
-        intakeMotorTwo.set(speed * -1);
-    }
-    
-/** 
-      public void noteRelease(double speed){
+    public void undertakerControl(double speed){
         intakeMotorOne.set(speed);
         intakeMotorTwo.set(speed);
     }
     
-      public void notePickup(double speed){
-        intakeMotorOne.set(speed * -1);
-        intakeMotorTwo.set(speed * -1);
-    }
-    */
 }
