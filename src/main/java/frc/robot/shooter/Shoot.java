9b95e4ff31fac719df6c55e175844be795ff7dab
Noah Dinan <noah@shinysocks.net>
2024-02-03 14:07:48 -0600

    switch from timers to built in withTimeout method for Shoot

--- src/main/java/frc/robot/shooter/Shoot.java
+++ src/main/java/frc/robot/shooter/Shoot.java
@@ -3,19 +3,15 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.shooter;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 
 public class Shoot extends Command {
     private ShooterSubsystem subsystem;
     private double targetSpeed;
-    private Timer timer;
 
     public Shoot(ShooterSubsystem subsystem, double targetSpeed) {
         this.subsystem = subsystem;
         this.targetSpeed = targetSpeed;
-        timer = new Timer();
         addRequirements(subsystem);
     }
 
@@ -23,7 +19,6 @@ public class Shoot extends Command {
     @Override
     public void initialize() {
         subsystem.setIndexerSpeed(targetSpeed);
-        timer.start();  
     }
 
     // Called every time the scheduler runs while the command is scheduled.
@@ -35,13 +30,11 @@ public class Shoot extends Command {
     public void end(boolean interrupted) {
         subsystem.setTargetShooterSpeed(0);
         subsystem.setIndexerSpeed(0);
-        timer.stop();
-        timer.reset();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return timer.hasElapsed(Constants.Shooter.WAIT_UNTIL_END_SECS);
+        return false;
     }
 }
