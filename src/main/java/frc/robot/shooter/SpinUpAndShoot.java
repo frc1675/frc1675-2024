4acfd1a40fd3806b803a99125b5feae99e094b16
Noah Dinan <noah@shinysocks.net>
2024-02-03 14:07:48 -0600

    switch from timers to built in withTimeout method for Shoot

--- src/main/java/frc/robot/shooter/SpinUpAndShoot.java
+++ src/main/java/frc/robot/shooter/SpinUpAndShoot.java
@@ -7,7 +7,7 @@ public class SpinUpAndShoot extends SequentialCommandGroup {
     public SpinUpAndShoot(ShooterSubsystem subsystem) {
         addCommands(
             new SpinUp(subsystem, Constants.Shooter.TARGET_SHOOTER_SPEED),
-            new Shoot(subsystem, Constants.Shooter.TARGET_INDEXER_SPEED)
+            new Shoot(subsystem, Constants.Shooter.TARGET_INDEXER_SPEED).withTimeout(Constants.Shooter.WAIT_UNTIL_END_SECS)
         );
     }
 }
