4ae4a2248c32d06282027f0f45e77b7dc5c4b3ab
shinysocks <noah@shinysocks.net>
2024-02-03 09:12:45 -0600

    don't set values to 200

--- src/main/java/frc/robot/shooter/SpinUpAndShoot.java
+++ src/main/java/frc/robot/shooter/SpinUpAndShoot.java
@@ -5,8 +5,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class SpinUpAndShoot extends SequentialCommandGroup {
     public SpinUpAndShoot(ShooterSubsystem subsystem) {
         addCommands(
-            new SpinUp(subsystem, 200),
-            new Shoot(subsystem, 200)
+            new SpinUp(subsystem, 1),
+            new Shoot(subsystem, 1)
         );
     }
 }
