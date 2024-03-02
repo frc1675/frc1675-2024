682e47857db09627c5044084da3fe4e160cd475d
Noah Dinan <noah@shinysocks.net>
2024-03-02 11:30:10 -0600

    stopped setting actual motors in undertaker sim

--- src/main/java/frc/robot/undertaker/SimUndertaker.java
+++ src/main/java/frc/robot/undertaker/SimUndertaker.java
@@ -8,54 +8,54 @@ import frc.robot.Constants;
 
 public class SimUndertaker implements IUndertaker {
 
-  private final CANSparkMax intakeMotorOne = new CANSparkMax(Constants.Undertaker.INTAKE_MOTOR_ONE, MotorType.kBrushless);
-  private final CANSparkMax intakeMotorTwo = new CANSparkMax(Constants.Undertaker.INTAKE_MOTOR_TWO, MotorType.kBrushless);
-  private boolean[] mockMotorStatus = { false, false }; 
-  private double desiredSpeed;
-  private boolean hasRun = false;
+    private final CANSparkMax intakeMotorOne = new CANSparkMax(Constants.Undertaker.INTAKE_MOTOR_ONE,
+            MotorType.kBrushless);
+    private final CANSparkMax intakeMotorTwo = new CANSparkMax(Constants.Undertaker.INTAKE_MOTOR_TWO,
+            MotorType.kBrushless);
+    private boolean[] mockMotorStatus = { false, false };
+    private double desiredSpeed;
+    private boolean hasRun = false;
 
-  public SimUndertaker(){
-    REVPhysicsSim.getInstance().addSparkMax(intakeMotorOne, DCMotor.getNEO(1));
-    REVPhysicsSim.getInstance().addSparkMax(intakeMotorTwo, DCMotor.getNEO(1));
-  }
-
-  @Override
-  public double getDesiredSpeed(){
-    return desiredSpeed;
-  }
-
-  @Override
-  public double[] getMotorsOutput(){
-    double[] motorsOutput = new double[2];
-    motorsOutput[0] = intakeMotorOne.getAppliedOutput();
-    motorsOutput[1] = intakeMotorTwo.getAppliedOutput();
-    return motorsOutput;
-  }
-
-  @Override
-  public void run(double speed){
-    if (mockMotorStatus[0] && mockMotorStatus[1]) {
-      this.desiredSpeed = speed;
-      intakeMotorOne.setVoltage(speed * 12.0);
-      intakeMotorTwo.setVoltage(speed * 12.0);
-      hasRun = true;
+    public SimUndertaker() {
+        REVPhysicsSim.getInstance().addSparkMax(intakeMotorOne, DCMotor.getNEO(1));
+        REVPhysicsSim.getInstance().addSparkMax(intakeMotorTwo, DCMotor.getNEO(1));
     }
-  }
 
-  @Override
-  public boolean[] isAlive(){
-    return mockMotorStatus;
-  }
+    @Override
+    public double getDesiredSpeed() {
+        return desiredSpeed;
+    }
 
-  public void setIsAlive(boolean[] status){
-    mockMotorStatus = status;
-  }
+    @Override
+    public double[] getMotorsOutput() {
+        double[] motorsOutput = new double[2];
+        motorsOutput[0] = desiredSpeed;
+        motorsOutput[1] = desiredSpeed;
+        return motorsOutput;
+    }
 
-  public boolean getHasRun(){
-    return hasRun;
-  }
+    @Override
+    public void run(double speed) {
+        if (mockMotorStatus[0] && mockMotorStatus[1]) {
+            this.desiredSpeed = speed;
+            hasRun = true;
+        }
+    }    
 
-  public void resetHasRun(){
-    hasRun = false;
-  }
+    @Override
+    public boolean[] isAlive() {
+        return mockMotorStatus;
+    }
+
+    public void setIsAlive(boolean[] status) {
+        mockMotorStatus = status;
+    }
+
+    public boolean getHasRun() {
+        return hasRun;
+    }
+
+    public void resetHasRun() {
+        hasRun = false;
+    }
 }
