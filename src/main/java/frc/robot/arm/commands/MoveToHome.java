423879e36f5a841d7457f267b55de5355a0d7a23
Michael <mshalewski@outlook.com>
2024-03-06 18:26:40 -0600

    fixed command arm subsystem reference

--- src/main/java/frc/robot/arm/commands/MoveToHome.java
+++ src/main/java/frc/robot/arm/commands/MoveToHome.java
@@ -2,19 +2,11 @@ package frc.robot.arm.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
-<<<<<<< HEAD
 import frc.robot.arm.ArmSubsystem;
 
 public class MoveToHome extends Command {
   private ArmSubsystem arm;
-  public MoveToHome(ArmSubsystem arm) {
-=======
-import frc.robot.arm.Arm;
-
-public class MoveToHome extends Command {
-  private Arm arm;
-  public MoveToHome(Arm arm) {
->>>>>>> main
+  public MoveToHome(ArmSubystem arm) {
     this.arm = arm;
     addRequirements(arm);
   }
--- src/main/java/frc/robot/arm/commands/MoveToPosition.java
+++ src/main/java/frc/robot/arm/commands/MoveToPosition.java
@@ -3,21 +3,12 @@ package frc.robot.arm.commands;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
-<<<<<<< HEAD
 import frc.robot.arm.ArmSubsystem;
 public class MoveToPosition extends Command {
   private ArmSubsystem arm;
   private double targetAngle;
   private Debouncer debouncer;
   public MoveToPosition(ArmSubsystem arm, double targetAngle){
-=======
-import frc.robot.arm.Arm;
-public class MoveToPosition extends Command {
-  private Arm arm;
-  private double targetAngle;
-  private Debouncer debouncer;
-  public MoveToPosition(Arm arm, double targetAngle){
->>>>>>> main
     this.targetAngle = targetAngle;
     this.arm = arm;
     debouncer = new Debouncer(Constants.Arm.DEBOUNCE_TIME);
