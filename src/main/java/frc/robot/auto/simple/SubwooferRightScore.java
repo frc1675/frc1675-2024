0732932c1e76721f570817604df82b12895f1813
Noah Dinan <91911303+shinysocks@users.noreply.github.com>
2024-03-11 23:42:38 -0500

    move SpinUpAndShoot command (#74)

--- src/main/java/frc/robot/auto/simple/SubwooferRightScore.java
+++ src/main/java/frc/robot/auto/simple/SubwooferRightScore.java
@@ -4,9 +4,9 @@ import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.cmdGroup.IntakeNote;
-import frc.robot.cmdGroup.SpinUpAndShoot;
 import frc.robot.drive.DriveSubsystem;
 import frc.robot.shooter.ShooterSubsystem;
+import frc.robot.shooter.commands.SpinUpAndShoot;
 import frc.robot.undertaker.UndertakerSubsystem;
 import frc.robot.util.RobotContext;
 
