979e6dc7e259fb14e6564e4d3701534403d59e04
Noah Dinan <noah@shinysocks.net>
2024-03-13 18:37:08 -0500

    log the camera stats

--- src/main/java/frc/robot/util/DriverDashboard.java
+++ src/main/java/frc/robot/util/DriverDashboard.java
@@ -1,30 +1,32 @@
 package frc.robot.util;
 
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.cscore.VideoCamera;
+import edu.wpi.first.cscore.VideoMode;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class DriverDashboard {
+  private VideoCamera webCam;
 
   public DriverDashboard(RobotContext robotContext) {
     ShuffleboardTab tab = Shuffleboard.getTab("Driver");
-    VideoCamera webCam = getWebCam();
+    webCam = CameraServer.startAutomaticCapture();
+    webCamInit(webCam);
     tab.add(webCam).withPosition(0, 0).withSize(5, 5);
 
     // VideoCamera limelightCamera = getLimelight();
   }
 
-  private VideoCamera getWebCam() {
-    UsbCamera camera = CameraServer.startAutomaticCapture();
-    return camera;
-
+  private void webCamInit(VideoCamera camera) {
     // get and set camera flavor:
       // camera.setVideoMode(PixelFormat.kMJPEG, 320, 240, 60);
-      // VideoMode currentVideoMode = camera.getVideoMode();
-      // currentVideoMode.fps -> fps
-      // currentVideoMode.height -> height ...etc
+      VideoMode currentVideoMode = camera.getVideoMode();
+      System.out.print("FPS" + currentVideoMode.fps + ";" + "WIDTH x HEIGHT" + currentVideoMode.width + " x " + currentVideoMode.height);
+      System.out.print("FPS" + currentVideoMode.fps + ";" + "WIDTH x HEIGHT" + currentVideoMode.width + " x " + currentVideoMode.height);
+      System.out.print("FPS" + currentVideoMode.fps + ";" + "WIDTH x HEIGHT" + currentVideoMode.width + " x " + currentVideoMode.height);
+      System.out.print("FPS" + currentVideoMode.fps + ";" + "WIDTH x HEIGHT" + currentVideoMode.width + " x " + currentVideoMode.height);
+      System.out.print("FPS" + currentVideoMode.fps + ";" + "WIDTH x HEIGHT" + currentVideoMode.width + " x " + currentVideoMode.height);
   }
 
   private VideoCamera getLimelight() {
