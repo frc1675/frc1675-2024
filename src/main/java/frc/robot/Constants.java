e21173fc761935b809724b1a34d0344f5494f62d
Jake <herrmann.jake@protonmail.com>
2024-03-16 03:02:05 -0500

    Made arm angles relative to the home position

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,14 +50,13 @@ public class Constants {
         public static final double DEBOUNCE_TIME = 0.5;
         // Position constnats
         public static final double HOME_POSITION = 138;
-        public static final double HIGH_SCORE_POSITION = 60;
-        public static final double AMP_POSITION = 35;
-        public static final double MAX_ARM_RANGE_DEGREES = 20; // 13 degrees is vertical
+        public static final double LONG_SHOT_ANGLE = HOME_POSITION - 33;
+        public static final double HIGH_SCORE_POSITION = HOME_POSITION - 78;
+        public static final double AMP_POSITION = HOME_POSITION - 103;
+        public static final double MAX_ARM_RANGE_DEGREES = HOME_POSITION - 118; // 13 degrees is vertical
         // PID Profile constants
         public static final double MAXIMUM_VELOCITY = 150; // degrees per second
         public static final double MAXIMUM_ACCELERATION = 750; // degrees per second squared
-
-        public static final double LONG_SHOT_ANGLE = 105;
     }
 
     public class PathPlanner {
