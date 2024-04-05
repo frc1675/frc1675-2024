e981895342a757f7eccbe13d496b4c491daf1cd9
Jake <herrmann.jake@protonmail.com>
2024-04-05 12:35:40 -0500

    Change layout of shooter for driver dashboard

--- src/main/java/frc/robot/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/shooter/ShooterSubsystem.java
@@ -40,23 +40,19 @@ public class ShooterSubsystem extends SubsystemBase {
   private void ShuffleboardInit() {
     ShuffleboardTab tab = Shuffleboard.getTab(Constants.Shooter.SHUFFLEBOARD_TAB);
     tab.addDouble("Target Top Shooter Speed", () -> targetTopShooterSpeed).withPosition(0, 0).withSize(2, 1);
-    tab.addDouble("Target Bottom Shooter Speed", () -> targetBottomShooterSpeed).withPosition(1, 0).withSize(2, 1);
+    tab.addDouble("Target Bottom Shooter Speed", () -> targetBottomShooterSpeed).withPosition(0, 1).withSize(2, 1);
     tab.addDouble("Shooter 1 Speed", () -> shooterIO.getShooterSpeeds()[0]).withPosition(2, 0);
     tab.addDouble("Shooter 2 Speed", () -> shooterIO.getShooterSpeeds()[1]).withPosition(3, 0);
-    tab.addDouble("Shooter Speed Diff.", () -> shooterIO.getShooterSpeeds()[0] - shooterIO.getShooterSpeeds()[1])
-        .withPosition(4, 0).withSize(2, 1);
+    tab.addDouble("Shooter Speed Diff.", () -> shooterIO.getShooterSpeeds()[0] - shooterIO.getShooterSpeeds()[1]).withPosition(4, 0).withSize(2, 1);
 
-    tab.addBoolean("Shooter Ready?", () -> isShooterReady()).withPosition(6, 0);
-    tab.addBoolean("Has Note?", () -> isIndexerLoaded()).withPosition(6, 1);
+    tab.addBoolean("Shooter Ready?", () -> isShooterReady()).withPosition(3, 2).withSize(3, 3);
+    tab.addBoolean("Has Note?", () -> isIndexerLoaded()).withPosition(6, 1).withSize(4, 4);
 
-    tab.addDouble("Target Indexer Speed", () -> targetIndexerSpeed).withPosition(0, 1).withSize(2, 1);
+    tab.addDouble("Target Indexer Speed", () -> targetIndexerSpeed).withPosition(0, 2).withSize(2, 1);
     tab.addDouble("Indexer 1 Speed", () -> shooterIO.getIndexerSpeeds()[0]).withPosition(2, 1);
     tab.addDouble("Indexer 2 Speed", () -> shooterIO.getIndexerSpeeds()[1]).withPosition(3, 1);
-    tab.addDouble("Indexer Speed Diff.", () -> shooterIO.getIndexerSpeeds()[0] - shooterIO.getIndexerSpeeds()[1])
-        .withPosition(4, 1).withSize(2, 1);
-    tab.addDouble("LaserCAN Measurement", () -> shooterIO.getMeasurement());
-    tab.addDouble("Top Output", () -> topOutput);
-    tab.addDouble("Bottom Output", () -> bottomOutput);
+    tab.addDouble("Indexer Speed Diff.", () -> shooterIO.getIndexerSpeeds()[0] - shooterIO.getIndexerSpeeds()[1]).withPosition(4, 1).withSize(2, 1);
+    tab.addDouble("LaserCAN Measurement", () -> shooterIO.getMeasurement()).withPosition(0, 3).withSize(2, 1);
   }
 
   public boolean isIndexerLoaded() {
