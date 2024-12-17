9bc1d5b57f44559cb4bdc55f95dbf93a02403a2d
Josh Pordon <josh.pordon@gmail.com>
2024-12-16 18:16:52 -0600

    Use builder pattern

--- src/main/java/frc/robot/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/drive/DriveSubsystem.java
@@ -45,7 +45,7 @@ public class DriveSubsystem extends SubsystemBase {
     /**
      * Constructs a field-relative swerve drive subsystem that uses YAGSL and has needed methods for use with PathPlanner.
      * Additionally provides capability for automatically pointing to a specific heading.
-     * CAN IDs for drive motors, turning motors, and turning sensors are configured in the YAGSL json files at deploy/swerve.
+     * CAN IDs for drive motors; turning motors, and turning sensors are configured in the YAGSL json files at deploy/swerve.
      *
      * @param maxTranslationVelocity Maximum translation velocity of the robot in meters per second
      * @param maxRotationVelocity Maximum rotation velocity of the robot in radians per second
@@ -60,36 +60,25 @@ public class DriveSubsystem extends SubsystemBase {
      * @param pointingTolerance Tolerance for automatic pointing, in degrees.
      * @param maxVisionPoseOverrideDistance Maximum vision pose difference allowed, in meters. (If the vision pose is too far off, ignore it)
      */
-    public DriveSubsystem(
-            double maxTranslationVelocity,
-            double maxRotationVelocity,
-            double steeringGearRatio,
-            double steeringPulsePerRotation,
-            double driveGearRatio,
-            double drivePulsePerRotation,
-            double wheelDiameter,
-            double pointingP,
-            double pointingI,
-            double pointingD,
-            double pointingTolerance,
-            double maxVisionPoseOverrideDistance) {
+    private DriveSubsystem(DriveSubsystemBuilder builder) {
 
-        this.maxRotationVelocity = maxRotationVelocity;
-        this.maxTranslationVelocity = maxTranslationVelocity;
-        this.maxVisionPoseOverrideDistance = maxVisionPoseOverrideDistance;
+        this.maxRotationVelocity = builder.maxRotationVelocity;
+        this.maxTranslationVelocity = builder.maxTranslationVelocity;
+        this.maxVisionPoseOverrideDistance = builder.maxVisionPoseOverrideDistance;
 
-        rotationController = new PIDController(pointingP, pointingI, pointingD);
+        rotationController = new PIDController(builder.pointingP, builder.pointingI, builder.pointingD);
         rotationController.enableContinuousInput(-180, 180);
-        rotationController.setTolerance(pointingTolerance);
+        rotationController.setTolerance(builder.pointingTolerance);
 
         SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
         try {
             swerve = new SwerveParser(new File(Filesystem.getDeployDirectory(), "swerve"))
                     .createSwerveDrive(
                             maxTranslationVelocity,
-                            SwerveMath.calculateDegreesPerSteeringRotation(steeringGearRatio, steeringPulsePerRotation),
+                            SwerveMath.calculateDegreesPerSteeringRotation(
+                                    builder.steeringGearRatio, builder.steeringPulsePerRotation),
                             SwerveMath.calculateMetersPerRotation(
-                                    wheelDiameter, driveGearRatio, drivePulsePerRotation));
+                                    builder.wheelDiameter, builder.driveGearRatio, builder.drivePulsePerRotation));
         } catch (IOException e) {
             System.out.println("Swerve drive configuration file could not be found at "
                     + Filesystem.getDeployDirectory()
@@ -198,5 +187,136 @@ public class DriveSubsystem extends SubsystemBase {
                 speeds.omegaRadiansPerSecond,
                 false,
                 false);
+    }
+
+    public static class DriveSubsystemBuilder {
+        private double maxTranslationVelocity;
+        private double maxRotationVelocity;
+
+        private double steeringGearRatio;
+        private double steeringPulsePerRotation;
+
+        private double driveGearRatio;
+        private double drivePulsePerRotation;
+
+        private double wheelDiameter;
+
+        private double pointingP;
+        private double pointingI;
+        private double pointingD;
+        private double pointingTolerance;
+
+        private double maxVisionPoseOverrideDistance;
+
+        private boolean maxVelocitiesSet = false;
+        private boolean steeringPropertiesSet = false;
+        private boolean drivePropertiesSet = false;
+        private boolean wheelPropertiesSet = false;
+        private boolean pointingPidSet = false;
+        private boolean visionPropertiesSet = false;
+
+        /**
+         * Creates a builder to use
+         */
+        public DriveSubsystemBuilder() {}
+
+        /**
+         * Must be called to build a DriveSubsystem
+         * @param maxTranslationVelocity Maximum translation velocity of the robot in meters per second
+         * @param maxRotationVelocity Maximum rotation velocity of the robot in radians per second
+         */
+        public DriveSubsystemBuilder withMaxVelocities(double maxTranslationVelocity, double maxRotationVelocity) {
+            this.maxTranslationVelocity = maxTranslationVelocity;
+            this.maxRotationVelocity = maxRotationVelocity;
+            maxVelocitiesSet = true;
+            return this;
+        }
+
+        /**
+         * Must be called to build a DriveSubsystem
+         * @param steeringGearRatio Steering gear ratio (for reference, 12.8 in 2024)
+         * @param steeringPulsePerRotation PPR of the steering motor (1 if integrated, 1 in 2024)
+         * @return
+         */
+        public DriveSubsystemBuilder withSteeringMotorProperties(
+                double steeringGearRatio, double steeringPulsePerRotation) {
+            this.steeringGearRatio = steeringGearRatio;
+            this.steeringPulsePerRotation = steeringPulsePerRotation;
+            steeringPropertiesSet = true;
+            return this;
+        }
+
+        /**
+         * Must be called to build a DriveSubsystem
+         * @param driveGearRatio Drive gear ratio (for reference, 6.12 in 2024)
+         * @param drivePulsePerRotation PPR of the drive motor (1 if integrated, 1 in 2024)
+         * @return
+         */
+        public DriveSubsystemBuilder withDriveMotorProperties(double driveGearRatio, double drivePulsePerRotation) {
+            this.driveGearRatio = driveGearRatio;
+            this.drivePulsePerRotation = drivePulsePerRotation;
+            drivePropertiesSet = true;
+            return this;
+        }
+
+        /**
+         * Must be called to build a DriveSubsystem
+         * @param wheelDiameter Wheel diameter in meters
+         * @return
+         */
+        public DriveSubsystemBuilder withWheelDiameter(double wheelDiameter) {
+            this.wheelDiameter = wheelDiameter;
+            wheelPropertiesSet = true;
+            return this;
+        }
+
+        /**
+         * Must be called to build a DriveSubsystem
+         * @param pointingP P for automatic pointing rotation
+         * @param pointingI I for automatic pointing rotation
+         * @param pointingD D for automatic pointing rotation
+         * @param pointingTolerance Tolerance for automatic pointing, in degrees.
+         * @return
+         */
+        public DriveSubsystemBuilder withPointingPID(
+                double pointingP, double pointingI, double pointingD, double pointingTolerance) {
+            this.pointingP = pointingP;
+            this.pointingI = pointingI;
+            this.pointingD = pointingD;
+            this.pointingTolerance = pointingTolerance;
+            pointingPidSet = true;
+            return this;
+        }
+
+        /**
+         * Must be called to build a DriveSubsystem
+         * @param maxVisionPoseOverrideDistance Maximum vision pose difference allowed, in meters. (If the vision pose is too far off, ignore it)
+         * @return
+         */
+        public DriveSubsystemBuilder withVisionProperties(double maxVisionPoseOverrideDistance) {
+            this.maxVisionPoseOverrideDistance = maxVisionPoseOverrideDistance;
+            visionPropertiesSet = true;
+            return this;
+        }
+
+        /**
+         * Builds a DriveSubsystem as long as all the builder methods have been called.
+         * @return
+         * @throws RuntimeException if all the builder methods have not been called
+         */
+        public DriveSubsystem build() {
+            if (maxVelocitiesSet
+                    && steeringPropertiesSet
+                    && drivePropertiesSet
+                    && wheelPropertiesSet
+                    && pointingPidSet
+                    && visionPropertiesSet) {
+                return new DriveSubsystem(this);
+            } else {
+                // This is an unchecked exception that Java doesn't force us to catch
+                // that's fine because it should never happen, if it does happen the robot should crash.
+                throw new RuntimeException("Didn't fully initialize the builder");
+            }
+        }
     }
 }
